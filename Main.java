ff7d0f1ee5060ad2a9bad9f1f6aaa88d5748057e
Harsh312001 <102729661+Harsh312001@users.noreply.github.com>
2023-05-13 11:49:39 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,403 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.lang.Math;
+
+//public class Main {
+//    public static void main(String[] args)
+//    {
+//        //program 1 program to print an intergar from the user
+//        Scanner sc=new Scanner(System.in);
+//        int num =sc.nextInt();
+//        System.out.println(num);
+//
+//    }
+//}
+//program to add tw0 numbers
+//public class Main {
+//    public static void main(String[] args){
+//    Scanner sc=new Scanner (System.in);
+//        System.out.println("enter your first unum");
+//    int num1=sc.nextInt();
+//        System.out.println("enter your second number");
+//    int num2=sc.nextInt();
+//    int sum=num1+num2;
+//        System.out.println(sum);
+//    }
+//}
+
+//program to find the sum of two flaoting point numbers
+//public class Main {
+//    public static void main(String[] args){
+//        System.out.println("enter your first unum");
+//     Scanner sc=new Scanner(System.in);
+//    float num1=sc.nextInt();
+//        System.out.println("enter your second number");
+//    float num2=sc.nextInt();
+//    float sum=num1+num2;
+//        System.out.println(sum);
+//    }
+//}
+
+//java program to find the ascii character
+//
+//public class Main {
+//    public static void main(String[] args) {
+//    char word='A';
+//    int ascii=word;
+//        System.out.println("The ASCII value is =="+ascii);
+//    }
+//}
+
+//java program to cumpute quotient and remainder
+//public class Main {
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        int num1=sc.nextInt();
+//        int num2=sc.nextInt();
+//        int quo=num1/num2;
+//        int remainder=num1%num2;
+//        System.out.println("the quotient is"+quo);;
+//        System.out.println("The emainder is"+remainder);
+//    }}
+
+//6 prgram to swap two values in java
+//public class Main {
+//    public static void main(String[] args){
+//      Scanner sc=new Scanner(System.in);
+//      int num1=sc.nextInt();
+//      int num2=sc.nextInt();
+//        num1=num1+num2;
+//       num2=num1-num2;
+//        System.out.println("after swapping");
+//        System.out.println(num1);
+//        System.out.println(num2);
+//    }}
+
+//check the  number is even or odd
+//public class Main {
+//    public static void main(String[] args) {
+//    Scanner sc=new Scanner(System.in);
+//    int number=sc.nextInt();
+//    if(number>0)
+//        System.out.println("number is even");
+//    else {
+//        System.out.println("Number is odd");
+//    }}
+//    }
+
+//check the alpha bet is vowel or consonant
+//public class Main {
+//    public static void main(String[] args){
+//        int i=0;
+//        Scanner sc=new Scanner(System.in);
+//         char ch=sc.next().charAt(0);
+//        if(ch=="a"||ch=="e"||ch=="i"||ch=="o"||ch=="u"){
+//            System.out.println("it is characteer");}
+//        else{
+//            System.out.println("it is nt a character");
+//        }}}
+
+
+//program to find the lrgest amng three numbers
+//public class Main {
+//    public static void main(String[] args) {
+//    Scanner sc=new Scanner(System.in);
+//    int num1=sc.nextInt();
+//    int num2=sc.nextInt();
+//    int num3=sc.nextInt();
+//    if(num1>num2 && num1>num3)
+//    {
+//        System.out.println("num1 is greatest");
+//    }
+//    else if (num2>num3 && num2>num1)
+//        {
+//        System.out.println("num2 is greates");
+//    }
+//    else{
+//        System.out.println("num3 is greatest among three numbers");
+//    }
+//}
+//    }
+
+//program to find the quardric equation roots
+//public class Main {
+//    public static void main(String[] args){
+//      Scanner sc=new Scanner(System.in);
+//      int a=sc.nextInt();
+//      int b=sc.nextInt();
+//      int c=sc.nextInt();
+//      int d=
+//
+//    }}
+
+//wap to create a programm to find the frequency of the string
+//public class Main {
+//    public static void main(String[] args) {
+//    Scanner sc=new Scanner(System.in);
+//    String name=sc.nextLine();
+//    char ch=sc.next().charAt(1);
+//    for(int i=0;)
+//    }
+//    }
+
+//wap to check the number is leap year or not a leap year or not
+//public class Main {
+//    public static void main(String[] args){
+//        Scanner sc=new Scanner(System.in);
+//        int num=sc.nextInt();
+//        if(num%100==0&&num%4==0)
+//        {
+//            System.out.println("year is leap year");
+//        }
+//        else{
+//            System.out.println("not a leap year");
+//        }
+//    }}
+
+//wap to find the sum of the natural number upto n
+//public class Main {
+//    public static void main(String[] args){
+//        int sum=0;
+//    Scanner sc=new Scanner(System.in);
+//    int num=sc.nextInt();
+//    for(int i=0;i<=num;i++)
+//    {
+//        sum=sum+i;
+//    }
+//        System.out.println(sum);
+//    }
+//}
+
+
+//wap to find the factorial of the number which is input by the user
+//public class Main {
+//    public static void main(String[] args) {
+//        int s = 1;
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        for (int i = 1; i <= n; i++) {
+//            s = s * i;
+//        }
+//        System.out.println("The factorial of the number is "+ s);
+//    }
+//}
+
+//wap to create the table of the number
+//public class Main {
+//    public static void main(String[] args) {
+//        int s=1;
+// Scanner sc=new Scanner(System.in);
+// int n=sc.nextInt();
+// for(int i=1;i<=;i++)
+// {
+//      s=n*i;
+// }
+//        System.out.println(s);
+//}}
+
+////wap to print the fibonacci series////////////
+//public class Main {
+//    public static void main(String[] args) {
+//    int a=0,b=0,c=1;
+//    Scanner sc=new Scanner(System.in);
+//    int n=sc.nextInt();
+//    for(int i=0;i<=n;i++) {
+//   System.out.println(c);
+//        a = b;
+//        b = c;
+//        c=a+b;
+//        System.out.println(c);
+//    }}}
+
+//wap to find the gcd amd hcm of the two numbers
+//public class Main {
+//    public static void main(String[] args) {
+//        int h=1;
+//      Scanner sc=new Scanner(System.in);
+//      int a=sc.nextInt();
+//      int b=sc.nextInt();
+//      for(int i=1;i<=a||i<=b;i++)
+//      {
+//          if(a%i==0 &&  b%i==0){
+//             h=i;
+//      }}
+//        System.out.println(h);
+//      int l=a*b/h;
+//        System.out.println(l);
+//    }}
+
+//wap t print all alphabets A to z
+//public class Main{
+//    public static void main(String args[]){
+//        for(char i='A';i<='Z';i++){
+//            System.out.println(i);
+//        }
+//    }
+//}
+
+//wap to count the digits of the number
+//public class Main{
+//    public static void main(String args[]){
+//        int count=0;
+//        Scanner  sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        while(n>0)
+//        {
+//            count++;
+//            n=n/10;
+//        }
+//        System.out.println(count);
+//    }}
+
+//wap to reverse of the number
+//public class Main{
+//    public static void main(String args[]) {
+//        int r,s=0;
+//    Scanner sc=new Scanner(System.in);
+//    int n=sc.nextInt();
+//    while(n>0)
+//    {
+//        r=n%10;
+//        s=s*10+r;
+//        n=n/10;
+//
+//    }
+//        System.out.println(s);
+//    }
+//
+//    }
+
+//wap to check the number is armtrong or not
+//public class Main{
+//    public static void main(String args[]){
+//        int r,s=0;
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        int k=n;
+//       while(n>0)
+//       {
+//           r=n%10;
+//           s=s+r*r*r;
+//           n=n/10;
+//       }
+//        if(s==k){
+//            System.out.println("number is armstrong");
+//        }
+//        else{
+//            System.out.println("nummber is not armstrong");
+//        }
+//    }}
+
+//wap to check the number is palindrone or not
+//public class Main{
+//    public static void main(String args[]){
+//    int r,s=0;
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        int k=n;
+//        while(n>0)
+//        {
+//            r=n%10;
+//            s=s*10+r;
+//            n=n/10;
+//        }
+//
+//        if(s==k)
+//        {
+//            System.out.println("numer is palindrone");
+//        }
+//        else{
+//            System.out.println("number is not palindrone");
+//        }
+//
+//    }}
+
+
+//wap to check the number is prime oor not
+//public class Main{
+//    public static void main(String args[]){
+//        int k=0;
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        for(int i=1;i<=n;i++)
+//        {
+//            if(n%i==0)
+//               k++  ;
+//            }
+//
+//        if(k==2){
+//            System.out.println("number is prime number");
+//
+//        }
+//        else{
+//            System.out.println("number is not a prime number");
+//        }
+//
+//    }}
+
+//public class Main{
+//    public static void main(String args[]){
+//        for(int i=1;i=<5;i++)
+//        {
+//        for(int j=1;j<=i;j++){
+//            System.out.print("* ");
+//        }
+//            System.out.println();
+//    }}}
+
+//public class Main{
+//    public static void  main(String args[]){
+//        for(int i=1;i<6;++i)
+//        {
+//            for(int j=1;j<5;++j){
+//                if(i==3||j==2){
+//                    System.out.println("i="+i+";j="+j);
+//                }
+//            }
+//        }
+//
+//        }}
+//
+//class Main{
+//    public static void  main(String args[]){
+//        int a[]={1,2,3,4,5};
+//        for(int i=0 ;i<a.length;i++){
+//            System.out.println(a[i]);
+//        }
+//    }
+//}
+//copying n array fro onne to another
+//class Main {
+//    public static void main(String args[]) {
+//        int a[] = {1, 2, 3, 4, 5};
+//        int b[] = a;
+//        for (int i = 0; i < b.length; i++) {
+//            System.out.println("the number is" + b[i]);
+//
+//class Main{
+//    public static void main(String args[])
+//    {
+//        int a[]={12,3,4,5};
+//        int [] b=new int[4];
+//        for(int i=0;i<a.length;i++)
+//        {
+//            b[i]=a[i];
+//        }
+//        System.out.println(Arrays.toString(b));
+//    }
+//}
+
+class lamp{
+    void demo(){
+        System.out.println("hello");
+    }
+}
+public class Main {
+    public static void main(String args[]) {
+        lamp obj = new lamp();
+        obj.demo();
+        obj.demo();
+        obj.demo();
+
+    }
+}
